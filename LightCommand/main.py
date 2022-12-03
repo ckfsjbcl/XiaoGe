e7766888d007c387233dd3be68f680d8d4b1c073
ckfsjbcl <3270039695@qq.com>
2022-12-03 17:07:16 +0800

    更新

--- LightCommand/main.py
+++ LightCommand/main.py
@@ -8,17 +8,28 @@ import tools
 __version__ = 1.0
 
 
+def get_files_name(path):
+    return os.listdir(path)
+
+
 class LightCommand:
     def __init__(self, name='LightCommand'):
         # 初始化
-        if 'START.txt' not in self.get_files_name('.'):
+        if 'START.txt' not in get_files_name('.'):
             with open('START.txt', 'r+') as Startfile:
-                Startfile.write(f'Create Time:{tools.get_time_now__()}\n'
-                                f'Version:{__version__}'
-                                f'Name:{name}')
+                Startfile.write(f'* Create Time: {tools.get_time_now__()}\n'
+                                f'* Version: {__version__}\n'
+                                f'* Name: {name}\n'
+                                f'!本文件为启动必须,若删除则会覆盖所有已配置参数,若需重置则删除本文件即可!\n')
+                print("控制台配置文件重载中...")
 
-    def get_files_name(self, path):
-        return os.listdir(path)
+
+class FILES:
+    def __init__(self):
+        ...
+
+    def __get__(self, instance, owner):
+        ...
 
 
 Command = LightCommand()
